eb927aa0ac6e344bc350eb60977e7826aeebe9e0
knightingal <yujianming1985@hotmail.com>
2023-11-05 14:22:59 +0800

    add processer

--- lib/video_player.dart
+++ lib/video_player.dart
@@ -123,40 +123,42 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen>
           if (snapshot.connectionState == ConnectionState.done) {
             // If the VideoPlayerController has finished initialization, use
             // the data it provides to limit the aspect ratio of the video.
-            return Center(
-              child: AspectRatio(
-                key: globalKey,
-                aspectRatio: _controller.value.aspectRatio,
-                // Use the VideoPlayer widget to display the video.
-                child: Stack(
-                  children: [
-                    VideoPlayer(_controller),
-                    Text(widget.videoUrl,
-                        style: const TextStyle(color: Color(0xFF00FF00))),
-                    PlayerTimer(controller: _controller),
-                    GestureDetector(
-                      onTapDown: (e) {
-                        RenderBox box = globalKey.currentContext!
-                            .findRenderObject() as RenderBox;
-                        // log("onTap");
-                        // log("position ${e.localPosition}");
-                        // log("box ${box.size}");
-                        var x = e.localPosition.dx;
-                        var xTotal = box.size.width;
-                        var per = x / xTotal;
-                        var seekToSec =
-                            (_controller.value.duration.inSeconds * per)
-                                .toInt();
-                        _controller.seekTo(Duration(seconds: seekToSec));
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.all(0),
-                        color: Colors.transparent,
-                      ),
-                    )
-                  ],
+            _controller.play();
+            return Stack(
+              children: [
+                Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  ),
                 ),
-              ),
+                Processer(controller: _controller),
+
+                // VideoPlayer(_controller),
+                Text(widget.videoUrl,
+                    style: const TextStyle(color: Color(0xFF00FF00))),
+                PlayerTimer(controller: _controller),
+                GestureDetector(
+                  key: globalKey,
+                  onTapDown: (e) {
+                    RenderBox box = globalKey.currentContext!.findRenderObject()
+                        as RenderBox;
+                    // log("onTap");
+                    // log("position ${e.localPosition}");
+                    // log("box ${box.size}");
+                    var x = e.localPosition.dx;
+                    var xTotal = box.size.width;
+                    var per = x / xTotal;
+                    var seekToSec =
+                        (_controller.value.duration.inSeconds * per).toInt();
+                    _controller.seekTo(Duration(seconds: seekToSec));
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(0),
+                    color: Colors.transparent,
+                  ),
+                )
+              ],
             );
           } else {
             // If the VideoPlayerController is still initializing, show a
@@ -167,28 +169,107 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen>
           }
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Wrap the play or pause in a call to `setState`. This ensures the
-          // correct icon is shown.
-          setState(() {
-            // If the video is playing, pause it.
-            if (_controller.value.isPlaying) {
-              _controller.pause();
-            } else {
-              // If the video is paused, play it.
-              _controller.play();
-              // Duration position = _controller.value.position;
-              //
-              // _controller.seekTo(position + const Duration(minutes: 10));
-            }
-          });
-        },
-        // Display the correct icon depending on the state of the player.
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     // Wrap the play or pause in a call to `setState`. This ensures the
+      //     // correct icon is shown.
+      //     setState(() {
+      //       // If the video is playing, pause it.
+      //       if (_controller.value.isPlaying) {
+      //         _controller.pause();
+      //       } else {
+      //         // If the video is paused, play it.
+      //         _controller.play();
+      //         // Duration position = _controller.value.position;
+      //         //
+      //         // _controller.seekTo(position + const Duration(minutes: 10));
+      //       }
+      //     });
+      //   },
+      //   // Display the correct icon depending on the state of the player.
+      //   child: Icon(
+      //     _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+      //   ),
+      // ),
     );
+  }
+}
+
+class Processer extends StatefulWidget {
+  final VideoPlayerController controller;
+
+  const Processer({super.key, required this.controller});
+
+  @override
+  State<StatefulWidget> createState() {
+    return ProcesserState();
+  }
+}
+
+class ProcesserState extends State<Processer>
+    with SingleTickerProviderStateMixin {
+  late final Ticker _ticker;
+  Duration duration = const Duration();
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _ticker.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+
+    _ticker = createTicker((elapsed) {
+      // 4. update state
+      setState(() {
+        // _elapsed = elapsed;
+        duration = widget.controller.value.position;
+      });
+    });
+
+    _ticker.start();
+
+    // Initialize the controller and store the Future for later use.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
+    return CustomPaint(
+      size: Size(width, height),
+      painter: ProcesserPainter(
+          width: width *
+              (duration.inSeconds /
+                  widget.controller.value.duration.inSeconds)),
+    );
+  }
+}
+
+class ProcesserPainter extends CustomPainter {
+  final double width;
+
+  ProcesserPainter({required this.width});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.yellow
+      ..strokeWidth = 4;
+    canvas.drawLine(const Offset(0, 2), Offset(width, 2), paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter o) {
+    return false;
   }
 }
