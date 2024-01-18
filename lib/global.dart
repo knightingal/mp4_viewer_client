591908a2a9bdae6fdd3db018309567e0e6257afb
knightingal <yujianming1985@hotmail.com>
2024-01-18 20:53:11 +0800

    support apiVersion in MountConfig

--- lib/global.dart
+++ lib/global.dart
@@ -2,16 +2,21 @@ class MountConfig {
   final int id;
   final String baseDir;
   final String urlPrefix;
+  final int apiVersion;
 
   const MountConfig({
     required this.id,
     required this.baseDir,
     required this.urlPrefix,
+    required this.apiVersion,
   });
 
   factory MountConfig.fromJson(Map<String, dynamic> json) {
     return MountConfig(
-        id: json["id"], baseDir: json["baseDir"], urlPrefix: json["urlPrefix"]);
+        id: json["id"],
+        baseDir: json["baseDir"],
+        urlPrefix: json["urlPrefix"],
+        apiVersion: json["apiVersion"]);
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,15 @@ class Mp4ListPage extends StatefulWidget {
   State<Mp4ListPage> createState() => Mp4ListPageState();
 }
 
+class Mp4GridPage extends StatefulWidget {
+  const Mp4GridPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Mp4GridPage> createState() => Mp4GridPageState();
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -120,13 +129,23 @@ class MountConfigListState extends State<MountConfigListPage> {
               title: dirConfigList[index].baseDir,
               tapCallback: (int index, String title) {
                 selectedMountConfig = index;
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => Mp4ListPage(
-                            title: title,
-                          )),
-                );
+                if (dirConfigList[index].apiVersion == 1) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Mp4ListPage(
+                              title: title,
+                            )),
+                  );
+                } else {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Mp4GridPage(
+                              title: title,
+                            )),
+                  );
+                }
               });
         },
         prototypeItem: DirItem(
@@ -134,13 +153,23 @@ class MountConfigListState extends State<MountConfigListPage> {
           title: dirConfigList.first.baseDir,
           tapCallback: (int index, String title) {
             selectedMountConfig = index;
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => Mp4ListPage(
-                        title: title,
-                      )),
-            );
+            if (dirConfigList[index].apiVersion == 1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Mp4ListPage(
+                          title: title,
+                        )),
+              );
+            } else {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Mp4GridPage(
+                          title: title,
+                        )),
+              );
+            }
           },
         ),
         itemCount: dirConfigList.length,
@@ -160,6 +189,137 @@ class MountConfigListState extends State<MountConfigListPage> {
         body: Center(child: body),
       );
     }
+  }
+}
+
+class Mp4GridPageState extends State<Mp4GridPage> {
+  Future<List<String>> fetchDirs() async {
+    final response = await http.get(Uri.parse(
+        "${apiHost()}/mp4-dir/${gMountConfigList[selectedMountConfig!].id}/"));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonArray = jsonDecode(response.body);
+      List<String> dataList =
+          jsonArray.map((dynamic e) => e as String).toList();
+      return dataList;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
+  Future<List<String>> fetchSubDirs(String subDir) async {
+    final response = await http.get(Uri.parse(
+        "${apiHost()}/mp4-dir/${gMountConfigList[selectedMountConfig!].id}/$subDir"));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonArray = jsonDecode(response.body);
+      List<String> dataList =
+          jsonArray.map((dynamic e) => e as String).toList();
+      return dataList;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
+  late Future<List<String>> futureDataList;
+
+  @override
+  void initState() {
+    super.initState();
+    if (parent.isEmpty) {
+      futureDataList = fetchDirs();
+    } else {
+      futureDataList = fetchSubDirs(getSubDir());
+    }
+  }
+
+  static const platform = MethodChannel('flutter/startWeb');
+
+  String generateFileUrlByTitle(String title) =>
+      "${gatewayHost()}/${gMountConfigList[selectedMountConfig!].urlPrefix}/${getSubDir()}$title";
+
+  void itemTapCallback(int index, String title) {
+    if (title.endsWith(".mp4")) {
+      // String videoUrl =
+      //     "${gatewayHost()}/${gMountConfigList[selectedMountConfig!].urlPrefix}/${getSubDir()}$title";
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => VideoPlayerApp(
+                  videoUrl: generateFileUrlByTitle(title),
+                )),
+      );
+    } else if (title.endsWith(".png") || title.endsWith(".jpg")) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                ImageViewer(imageUrl: generateFileUrlByTitle(title)),
+          ));
+    } else {
+      parent.add(title);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Mp4ListPage(
+                  title: widget.title,
+                )),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+
+    if (parent.isNotEmpty) {
+      parent.removeLast();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget body;
+    body = FutureBuilder<List<String>>(
+        future: futureDataList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                prototypeItem: DirItem(
+                  index: 0,
+                  title: snapshot.data!.first,
+                  tapCallback: itemTapCallback,
+                ),
+                itemBuilder: (context, index) {
+                  return DirItem(
+                    index: index,
+                    title: snapshot.data![index],
+                    tapCallback: itemTapCallback,
+                  );
+                });
+          } else {
+            return const Text("");
+          }
+        });
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(""),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          platform.invokeMethod("aboutPage");
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.arrow_back_sharp),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(child: body),
+    );
   }
 }
 
