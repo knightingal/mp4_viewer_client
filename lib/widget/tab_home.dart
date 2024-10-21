b33d21c537c698305f32a3f03f685f1db520b663
knightingal <yujianming1985@hotmail.com>
2024-10-21 23:07:10 +0800

    create TagMainPage

---
+++ lib/widget/tab_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TagMainPage extends StatelessWidget {
+  const TagMainPage({super.key});
+
+  @override
+  Widget build(Object context) {
+    return const Text("tag page");
+  }
+}
