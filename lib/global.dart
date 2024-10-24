f6936f3dba632fe5a7d4dbe0c648965822253c06
knightingal <yujianming1985@hotmail.com>
2024-10-24 23:35:01 +0800

    update `TagMainPage`

--- lib/global.dart
+++ lib/global.dart
@@ -43,6 +43,16 @@ class VideoInfo {
   }
 }
 
+class Tag {
+  final int id;
+  final String tag;
+  const Tag({required this.id, required this.tag});
+
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return Tag(id: json["id"], tag: json["tag"]);
+  }
+}
+
 late List<MountConfig> gMountConfigList;
 
 int? selectedMountConfig;
--- lib/widget/tag_home.dart
+++ lib/widget/tag_home.dart
@@ -1,17 +1,73 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
 
-class TagMainPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mp4_viewer_client/dir_item.dart';
+
+import '../global.dart';
+import '../main.dart';
+
+class TagMainPage extends StatefulWidget {
   const TagMainPage({super.key});
 
   @override
-  Widget build(Object context) {
+  State<TagMainPage> createState() {
+    return TagMainState();
+  }
+}
+
+class TagMainState extends State<TagMainPage> {
+  Future<List<Tag>> fetchSubDirs() async {
+    final response = await http.get(Uri.parse("${apiHost()}/query-tags"));
+    if (response.statusCode == 200) {
+      log(response.body);
+      List<dynamic> jsonArray = jsonDecode(response.body);
+      List<Tag> dataList = jsonArray.map((e) => Tag.fromJson(e)).toList();
+
+      return dataList;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
+  late Future<List<Tag>> futureDataList;
+
+  @override
+  void initState() {
+    super.initState();
+    futureDataList = fetchSubDirs();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget body;
+    body = FutureBuilder<List<Tag>>(
+        future: futureDataList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              prototypeItem: DirItem(
+                  index: 0,
+                  title: snapshot.data!.first.tag,
+                  tapCallback: (int index, String title) {}),
+              itemBuilder: (context, index) {
+                return DirItem(
+                    index: index,
+                    title: snapshot.data![index].tag,
+                    tapCallback: (int index, String title) {});
+              },
+            );
+          } else {
+            return const Text("tag");
+          }
+        });
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-      body: const Center(
-        child: Text("tag page"),
+      body: Center(
+        child: body,
       ),
     );
   }
