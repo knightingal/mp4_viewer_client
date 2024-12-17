b9cdc8f0078f17fc79fbfb492de7d39523451c9a
knightingal <yujianming1985@hotmail.com>
2024-12-17 19:22:49 +0800

    fix cfb16 decrypting

--- lib/widget/encript_image.dart
+++ lib/widget/encript_image.dart
@@ -22,19 +22,16 @@ class EncriptImageState extends State<EncriptImageWidget> {
     final key = encrypt_lib.Key.fromUtf8("password");
     final iv = encrypt_lib.IV.fromUtf8("");
     final encrypter = encrypt_lib.Encrypter(
-        encrypt_lib.AES(key, mode: encrypt_lib.AESMode.cfb64, padding: null));
+        encrypt_lib.AES(key, mode: encrypt_lib.AESMode.cfb128, padding: null));
     final reps2Future =
-        http.get(Uri.parse("http://192.168.2.12:8000/dev/image"));
+        http.get(Uri.parse("http://192.168.2.12:8000/dev/aes-image"));
     return reps2Future.then((resp2) {
       if (resp2.statusCode == 200) {
         Uint8List bytes = resp2.bodyBytes;
-        // log(bytes.length.toString());
-        // return bytes;
         List<int> body =
             encrypter.decryptBytes(encrypt_lib.Encrypted(bytes), iv: iv);
         Uint8List i8list = Uint8List.fromList(body);
-        log(i8list.length.toString());
-        return i8list.sublist(0, 158728);
+        return i8list;
       } else {
         return Uint8List.fromList([]);
       }
