824dba86049e3889dfa5e94de6334160b1b32f51
knightingal <yujianming1985@hotmail.com>
2025-06-02 10:50:31 +0800

    remove unused code

--- lib/widget/search_page.dart
+++ lib/widget/search_page.dart
@@ -1,7 +1,7 @@
+import 'dart:developer' show log;
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import "package:mp4_viewer_client/encrypt_lib.dart" as encrypt_lib;
 
 import 'package:http/http.dart' as http;
 
@@ -17,49 +17,24 @@ class SearchPage extends StatefulWidget {
 class SearchPageState extends State<SearchPage> {
   late Future<Uint8List> decriptedContentFuture;
 
-  Future<Uint8List> fetchImage() async {
-    final key = encrypt_lib.Key.fromUtf8("password");
-    final iv = encrypt_lib.IV.fromUtf8("");
-    final encrypter = encrypt_lib.Encrypter(
-      encrypt_lib.AES(key, mode: encrypt_lib.AESMode.cfb, padding: null),
-    );
-    final reps2Future = http.get(
-      Uri.parse("http://192.168.2.12:8000/dev/aes-image"),
-    );
-    return reps2Future.then((resp2) {
-      if (resp2.statusCode == 200) {
-        Uint8List bytes = resp2.bodyBytes;
-        List<int> body = encrypter.decryptBytes(
-          encrypt_lib.Encrypted(bytes),
-          iv: iv,
-        );
-        Uint8List i8list = Uint8List.fromList(body);
-        return i8list;
-      } else {
-        return Uint8List.fromList([]);
-      }
-    });
-  }
-
   @override
   void initState() {
     super.initState();
-    decriptedContentFuture = fetchImage();
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget body = FutureBuilder<Uint8List>(
-      future: decriptedContentFuture,
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-          return Image.memory(snapshot.data!);
-        } else {
-          return const Text("");
-        }
-      },
-    );
+    Widget body = SizedBox.shrink();
 
-    return body;
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          log("search button");
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.search),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(child: body),
+    );
   }
 }
