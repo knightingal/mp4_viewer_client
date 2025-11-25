a1de244ef577d2ff7867a11bf1ea11188583c683
knightingal <yujianming1985@hotmail.com>
2025-11-25 21:11:21 +0800

    remove ktor

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -125,9 +125,9 @@ task("releaseUpload") {
 }
 dependencies {
 
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.cio)
-    implementation(libs.ktor.client.okhttp)
+//    implementation(libs.ktor.client.core)
+//    implementation(libs.ktor.client.cio)
+//    implementation(libs.ktor.client.okhttp)
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- android/app/src/main/kotlin/com/example/mp4_viewer_client/tasks/ConcurrencyApkTask.kt
+++ android/app/src/main/kotlin/com/example/mp4_viewer_client/tasks/ConcurrencyApkTask.kt
@@ -2,10 +2,10 @@ package com.example.mp4_viewer_client.tasks
 
 import android.util.Log
 import com.example.mp4_viewer_client.AboutActivity
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.request.get
+//import io.ktor.client.HttpClient
+//import io.ktor.client.call.body
+//import io.ktor.client.engine.okhttp.OkHttp
+//import io.ktor.client.request.get
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.Response
@@ -35,19 +35,19 @@ object ConcurrencyApkTask {
         return withContext(Dispatchers.IO) {
             Log.d(TAG, "start download $src")
 
-            val client = makeClient(listener)
-            while (true) {
-                try {
-                    val bytes: ByteArray = client.get(src).body()
-                    val fileOutputStream = FileOutputStream(dest, false)
-                    fileOutputStream.write(bytes)
-                    fileOutputStream.close()
-                    break
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                    Log.e(TAG, "download $src error")
-                }
-            }
+//            val client = makeClient(listener)
+//            while (true) {
+//                try {
+//                    val bytes: ByteArray = client.get(src).body()
+//                    val fileOutputStream = FileOutputStream(dest, false)
+//                    fileOutputStream.write(bytes)
+//                    fileOutputStream.close()
+//                    break
+//                } catch (e: IOException) {
+//                    e.printStackTrace()
+//                    Log.e(TAG, "download $src error")
+//                }
+//            }
 
         }
 
