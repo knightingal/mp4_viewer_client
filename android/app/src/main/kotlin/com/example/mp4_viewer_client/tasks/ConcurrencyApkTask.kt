b580fac8de8552eeecd0ae80ebd4321177992f4e
knightingal <yujianming1985@hotmail.com>
2025-11-26 23:07:17 +0800

    support file download only by okhttp

--- android/app/src/main/kotlin/com/example/mp4_viewer_client/tasks/ConcurrencyApkTask.kt
+++ android/app/src/main/kotlin/com/example/mp4_viewer_client/tasks/ConcurrencyApkTask.kt
@@ -2,12 +2,10 @@ package com.example.mp4_viewer_client.tasks
 
 import android.util.Log
 import com.example.mp4_viewer_client.AboutActivity
-//import io.ktor.client.HttpClient
-//import io.ktor.client.call.body
-//import io.ktor.client.engine.okhttp.OkHttp
-//import io.ktor.client.request.get
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.Response
 import okio.Buffer
 import okio.BufferedSource
@@ -15,7 +13,6 @@ import okio.ForwardingSource
 import okio.buffer
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
 
 object ConcurrencyApkTask {
     private const val TAG = "DLImageTask"
@@ -31,9 +28,24 @@ object ConcurrencyApkTask {
 //        }
 //    }
 
+    private fun makeClient(listener: AboutActivity.DownloadCounterListener): OkHttpClient {
+        return OkHttpClient.Builder().addNetworkInterceptor { chain ->
+            val originalResponse: Response = chain.proceed(chain.request())
+            val body = originalResponse.body
+            val wrappedBody = ResponseBodyListener(body, listener)
+            originalResponse.newBuilder().body(wrappedBody).build()
+        }.build()
+    }
+
     suspend fun downloadToFile(src: String, dest: File, listener: AboutActivity.DownloadCounterListener) {
         return withContext(Dispatchers.IO) {
             Log.d(TAG, "start download $src")
+            val client = makeClient(listener)
+            val request = Request.Builder().url(src).build()
+            val bytes: ByteArray = client.newCall(request).execute().body.bytes()
+            val fileOutputStream = FileOutputStream(dest, false)
+            fileOutputStream.write(bytes)
+            fileOutputStream.close()
 
 //            val client = makeClient(listener)
 //            while (true) {
@@ -68,7 +80,7 @@ class ResponseBodyListener(val origin: okhttp3.ResponseBody, listener: AboutActi
     val byteCounter = ByteCounter(contentLength(), listener)
 
     override fun contentLength(): Long {
-        return origin.contentLength() ?: 0
+        return origin.contentLength()
     }
 
     private var bufferedSource: BufferedSource? = null
