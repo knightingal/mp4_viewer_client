460744e367520e97b39742ae54b186aac91e7d90
knightingal <yujianming1985@hotmail.com>
2025-11-29 23:06:20 +0800

    add VideoActivity

---
+++ android/app/src/main/kotlin/com/example/mp4_viewer_client/VideoActivity.kt
@@ -0,0 +1,39 @@
+package com.example.mp4_viewer_client
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_video)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val player = ExoPlayer.Builder(this).build()
+        val playerView = findViewById<PlayerView>(R.id.player_view)
+        playerView.player = player
+
+        val mediaItem = MediaItem.fromUri("http://192.168.2.12:8082/demo-video/1/demo/demp.mp4")
+        // Set the media item to be played.
+        player.setMediaItem(mediaItem)
+        // Prepare the player.
+        player.prepare()
+        // Start the playback.
+        player.play()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
