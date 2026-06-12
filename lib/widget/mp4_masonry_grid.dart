1ba8c494b58e573f04b65c6c4947b1711f475579
knightingal <yujianming1985@hotmail.com>
2026-06-12 21:59:44 +0800

    add GridItem in Mp4MasonryGrid

--- lib/widget/mp4_masonry_grid.dart
+++ lib/widget/mp4_masonry_grid.dart
@@ -1,9 +1,14 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:mp4_viewer_client/image_viewer.dart';
 import 'package:mp4_viewer_client/widget/common.dart';
+import 'package:mp4_viewer_client/widget/video_tag.dart';
 
 import '../global.dart';
 import '../main.dart';
@@ -212,4 +217,373 @@ class _Mp4MasonryGridState extends State<Mp4MasonryGrid> {
       body: Center(child: body),
     );
   }
+}
+
+class GridItem extends StatefulWidget {
+  final String title;
+  final String coverUrl;
+  final Rate? rate;
+
+  final int index;
+  final int videoId;
+  final int baseIndex;
+  final String dirPath;
+  final void Function() refreshCallback;
+
+  final bool showDuplicateDelMenu;
+
+  final String? designationChar;
+  final String? designationNum;
+
+  const GridItem({
+    super.key,
+    required this.index,
+    required this.videoId,
+    required this.title,
+    required this.coverUrl,
+    required this.rate,
+    required this.refreshCallback,
+    required this.baseIndex,
+    required this.dirPath,
+    this.designationChar,
+    this.designationNum,
+    this.showDuplicateDelMenu = false,
+  });
+
+  String generateFileUrlByTitle() {
+    var videoUrl = "${apiHost()}/video-stream-by-id/$videoId/stream.mp4";
+    log(videoUrl);
+    return videoUrl;
+  }
+
+  String generateVideoExistUrlByTitle() {
+    var videoUrl = "${apiHost()}/video-exist/$baseIndex$dirPath/$title";
+    return videoUrl;
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return GridState();
+  }
+}
+
+class GridState extends State<GridItem> {
+  static const platform = MethodChannel('flutter/startWeb');
+  Future<bool> checkExist() async {
+    final response = await http.get(
+      Uri.parse(widget.generateVideoExistUrlByTitle()),
+    );
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool exist = true;
+
+  @override
+  void initState() {
+    super.initState();
+    checkExist().then((exist) {
+      setState(() {
+        this.exist = exist;
+      });
+    });
+  }
+
+  void _startPreview() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            ImageViewer(imageUrl: widget.coverUrl, videoTitle: widget.title),
+      ),
+    );
+  }
+
+  void _startPlayer() {
+    if (Platform.isLinux) {
+      // execute on linux desktop
+      // open mpv player
+      Process.run("mpv", [widget.generateFileUrlByTitle()]).then((result) {
+        log("mpv exited with code ${result.exitCode}");
+      });
+    } else {
+      platform.invokeMethod("startVideo", {
+        "videoUrl": widget.generateFileUrlByTitle(),
+        "coverUrl": widget.coverUrl,
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    late Rate selectedItem;
+    if (widget.rate != null) {
+      selectedItem = widget.rate!;
+    } else {
+      selectedItem = Rate.none;
+    }
+
+    Color color = rateToColor(
+      selectedItem,
+      Theme.of(context).colorScheme.inversePrimary,
+    );
+    return Container(
+      padding: const EdgeInsets.all(0),
+      child: Card(
+        color: Theme.of(context).colorScheme.surface,
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: color, width: 2),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: SizedBox.expand(
+                child: GestureDetector(
+                  onLongPress: () => _startPreview(),
+                  onTapUp: (e) => _startPlayer(),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12.0),
+                    child: Hero(
+                      tag: "video-cover-${widget.coverUrl}",
+                      child: Image.network(widget.coverUrl, fit: BoxFit.fill),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 0,
+              child: GridTitleBar(
+                title: widget.title,
+                videoId: widget.videoId,
+                rate: widget.rate,
+                refreshCallback: widget.refreshCallback,
+                designationChar: widget.designationChar,
+                designationNum: widget.designationNum,
+                exist: exist,
+                showDuplicateDelMenu: widget.showDuplicateDelMenu,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GridTitleBar extends StatelessWidget {
+  final String title;
+  final int videoId;
+  final Rate? rate;
+  final void Function() refreshCallback;
+  final String? designationChar;
+  final String? designationNum;
+  final bool exist;
+  final bool showDuplicateDelMenu;
+
+  const GridTitleBar({
+    super.key,
+    required this.title,
+    required this.videoId,
+    required this.rate,
+    required this.refreshCallback,
+    required this.showDuplicateDelMenu,
+    this.designationChar,
+    this.designationNum,
+    this.exist = true,
+  });
+
+  void postRate(GridItemMenuItem item) async {
+    final response = await http.post(
+      Uri.parse("${apiHost()}/video-rate/$videoId/${item.index}"),
+    );
+    if (response.statusCode == 200) {
+      refreshCallback();
+    } else {
+      log("failed to post rate, ${response.statusCode}", error: response);
+    }
+  }
+
+  void deleteVideo() async {
+    final response = await http.delete(
+      Uri.parse("${apiHost()}/video/$videoId?duplicate_del=false"),
+    );
+    if (response.statusCode == 200) {
+      refreshCallback();
+    } else {
+      log("failed to delete video, ${response.statusCode}", error: response);
+    }
+  }
+
+  void deleteDuplicateVideo() async {
+    final response = await http.delete(
+      Uri.parse("${apiHost()}/video/$videoId?duplicate_del=true"),
+    );
+    if (response.statusCode == 200) {
+      refreshCallback();
+    } else {
+      log("failed to delete video, ${response.statusCode}", error: response);
+    }
+  }
+
+  void nav2TagHome(BuildContext context, int videoId) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => VideoTagPage(videoId: videoId)),
+    );
+  }
+
+  void nav2DetailPage(BuildContext context, int videoId) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MetaPage(id: videoId)),
+    );
+  }
+
+  PopupMenuButton<GridItemMenuItem> generatePopupMenuItems(
+    BuildContext context,
+  ) {
+    void onMenuItemSelected(GridItemMenuItem item) {
+      // Handle menu item selection
+      switch (item) {
+        case GridItemMenuItem.bad ||
+            GridItemMenuItem.good ||
+            GridItemMenuItem.normal:
+          postRate(item);
+        case GridItemMenuItem.tag:
+          nav2TagHome(context, videoId);
+        case GridItemMenuItem.detail:
+          nav2DetailPage(context, videoId);
+        case GridItemMenuItem.duplicate:
+          if (designationChar != null && designationNum != null) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Mp4MasonryGrid(
+                  title: "Duplicate of ${designationChar!}-${designationNum!}",
+                  searchWord: "${designationChar!}-${designationNum!}",
+                ),
+              ),
+            );
+          }
+        case GridItemMenuItem.delete:
+          deleteVideo();
+        case GridItemMenuItem.duplicateDel:
+          deleteVideo();
+        default:
+        // do nothing
+      }
+    }
+
+    return PopupMenuButton<GridItemMenuItem>(
+      onSelected: onMenuItemSelected,
+      itemBuilder: (BuildContext context) {
+        var items = <PopupMenuEntry<GridItemMenuItem>>[
+          const PopupMenuItem<GridItemMenuItem>(
+            value: GridItemMenuItem.good,
+            child: Text('Good'),
+          ),
+          const PopupMenuItem<GridItemMenuItem>(
+            value: GridItemMenuItem.normal,
+            child: Text('Normal'),
+          ),
+          const PopupMenuItem<GridItemMenuItem>(
+            value: GridItemMenuItem.bad,
+            child: Text('Bad'),
+          ),
+          const PopupMenuDivider(),
+          const PopupMenuItem<GridItemMenuItem>(
+            value: GridItemMenuItem.tag,
+            child: Text("Tag"),
+          ),
+          const PopupMenuItem<GridItemMenuItem>(
+            value: GridItemMenuItem.detail,
+            child: Text("Meta Detail"),
+          ),
+          const PopupMenuItem<GridItemMenuItem>(
+            value: GridItemMenuItem.duplicate,
+            child: Text("Find Duplicate"),
+          ),
+        ];
+        if (rate == Rate.bad) {
+          items.add(
+            const PopupMenuItem<GridItemMenuItem>(
+              value: GridItemMenuItem.delete,
+              child: Text('Delete'),
+            ),
+          );
+        }
+        if (showDuplicateDelMenu) {
+          items.add(
+            const PopupMenuItem<GridItemMenuItem>(
+              value: GridItemMenuItem.duplicateDel,
+              child: Text('Hide from Duplicate Search'),
+            ),
+          );
+        }
+        return items;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Rate selectedItem = rate ?? Rate.none;
+
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+      height: 40,
+      alignment: Alignment.centerLeft,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Text(
+              title,
+              style: TextStyle(
+                color: rateToColor(
+                  selectedItem,
+                  Theme.of(context).colorScheme.inversePrimary,
+                ),
+                decoration: TextDecoration.lineThrough,
+                decorationColor: Colors.black,
+                decorationStyle: TextDecorationStyle.solid,
+                decorationThickness: exist ? 0 : 2,
+              ),
+            ),
+          ),
+          Expanded(flex: 0, child: generatePopupMenuItems(context)),
+        ],
+      ),
+    );
+  }
+}
+
+Color rateToColor(Rate rate, Color defaultColor) {
+  return switch (rate) {
+    Rate.bad => Colors.red as Color,
+    Rate.normal => Colors.blue as Color,
+    Rate.good => Colors.green as Color,
+    Rate.deleted => Colors.grey as Color,
+    _ => defaultColor,
+  };
+}
+
+enum GridItemMenuItem {
+  none,
+  good,
+  normal,
+  bad,
+  tag,
+  detail,
+  duplicate,
+  delete,
+  duplicateDel,
 }
