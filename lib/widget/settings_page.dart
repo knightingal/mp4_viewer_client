09324a23993f51b634311cf47cac57afe2644f6b
knightingal <yujianming1985@hotmail.com>
2026-08-04 21:25:46 +0800

    set backgroundColor for settings page

--- lib/widget/settings_page.dart
+++ lib/widget/settings_page.dart
@@ -9,7 +9,10 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Settings")),
+      appBar: AppBar(
+        title: const Text("Settings"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
       body: SettingsWidget(),
     );
   }
