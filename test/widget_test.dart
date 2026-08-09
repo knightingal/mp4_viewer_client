4320703df865a58f992f7cc46658f02955c74c1c
knightingal <yujianming1985@hotmail.com>
2026-08-09 22:58:37 +0800

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,7 +16,7 @@ import 'package:mp4_viewer_client/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
